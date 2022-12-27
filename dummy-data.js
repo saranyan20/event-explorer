cb935d226f0b41f30f0b5e342dd781544b36c2d7
saranyan20 <srisaran2000@gmail.com>
2022-12-27 10:33:34 +0530

    feat: json data

--- dummy-data.js
+++ dummy-data.js
@@ -44,7 +44,9 @@ export function getFilteredEvents(dateFilter) {
 
   let filteredEvents = DUMMY_EVENTS.filter((event) => {
     const eventDate = new Date(event.date);
-    return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1;
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
   });
 
   return filteredEvents;
